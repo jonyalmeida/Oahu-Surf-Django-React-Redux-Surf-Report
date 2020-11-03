b148d532f58389d594438e686ad948884191950c
jon <jon@jons-MacBook-Pro.local>
2020-11-03 11:18:09 -0600

    add JWT token refresh views and urls

--- djsr/authentication/urls.py
+++ djsr/authentication/urls.py
@@ -1,16 +1,8 @@
 from django.urls import path
 from rest_framework_simplejwt import views as jwt_views
+from .views import ObtainTokenPairWithEmailView
 
 urlpatterns = [
-    path(
-        'token/obtain/',
-        jwt_views.TokenObtainPairView.as_view(),
-        name='token_create'
-    ),
-    # Override sjwt stock token
-    path(
-        'token/refresh/',
-        jwt_views.TokenRefreshView.as_view(),
-        name='token_refresh'
-    ),
+    path('token/obtain/', ObtainTokenPairWithEmailView.as_view(), name='token_create'),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ]
