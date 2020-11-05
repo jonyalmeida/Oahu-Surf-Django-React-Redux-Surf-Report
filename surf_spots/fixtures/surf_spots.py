6b2263d0f988a122c90cc1864e656a5a5a0ca0df
jon <jon@jons-MacBook-Pro.local>
2020-11-05 08:48:45 -0600

    add surfspots app+model, favorites realtional table

---
+++ surf_spots/fixtures/surf_spots.py
@@ -0,0 +1,10 @@
+[
+    {
+        "model": "surf_spots.surfspots",
+        "pk": 1,
+        "fields": {
+            "name": "Mick",
+            "shore": "Fanning"
+        }
+    },
+]
--- surf_spots/models.py
+++ surf_spots/models.py
@@ -1,13 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class SurfSpots(models.Model):
     name = models.CharField(max_length=50)
     shore = models.CharField(max_length=5)
-    lat = models.FloatField
-    long = models.FloatField
+    lat = models.FloatField(default=0.00000)
+    long = models.FloatField(default=0.00000)
     created_on = models.DateField(auto_now_add=True)
     updated_on = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{(self.name)}'
 
     def get_coords(self):
         return {
@@ -18,3 +22,10 @@ class SurfSpots(models.Model):
     def update_updated_on(self, newDate):
         self.updated_on = newDate
         self.save(['updated_on'])
+
+
+class FavoriteSpots(models.Model):
+    user = models.ForeignKey(
+        User, related_name='favorite_spots', null=False, on_delete=models.CASCADE)
+    surf_spot = models.ForeignKey(
+        SurfSpots, null=False, on_delete=models.CASCADE)
